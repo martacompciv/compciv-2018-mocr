bb900dc9d41c300b1dd7620bb95b47f73c72414a
martacompciv <martacompciv@gmail.com>
2018-02-12 18:56:26 -0800

    first files

--- week-05/txdeathrow_check/checker.py
+++ week-05/txdeathrow_check/checker.py
@@ -2,12 +2,12 @@ from data_helper import get_html
 from bs4 import BeautifulSoup
 txt = get_html()
 soup = BeautifulSoup(txt, 'lxml')
-tags = soup.select('tr')
+rows = soup.select('tr')
 
 def get_and_parse_inmate_rows():
     
-    return tags
+    return rows[1:]
 
 def count_inmates():
     
-    return len(tags)
+    return len(rows[1:])
